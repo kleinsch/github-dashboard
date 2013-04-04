e19b4b2e4ad87e53642e1d3f6f4b13faa2c947fe
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-04-04 15:21:05 -0700

    Ember.js and Grunt build process working

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    copy: {
+      app: {
+        files: [
+          {expand: true, cwd: 'app/static/', src: ['**'], dest: 'dist/', filter: 'isFile'},
+          {expand: true, cwd: 'vendor/static/', src: ['**'], dest: 'dist/', filter: 'isFile'}
+        ]
+      }
+    },
+
+    jshint: {
+      files: ['Gruntfile.js', 'app/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+
+    neuter: {
+      options: {
+        includeSourceURL: true
+      },
+      'dist/<%= pkg.name %>.js': 'app/app.js'
+    },
+
+    less: {
+      options: {
+        paths: ["app/stylesheets", "vendor/stylesheets"]
+      },
+      app: {
+        files: {
+          "dist/app.css": "app/app.less"
+        }
+      }
+    },
+
+    cssmin: {
+      compress: {
+        files: {
+          'dist/github-dashboard.css': [
+            'vendor/stylesheets/normalize.css',
+            'dist/app.css'
+          ]
+        }
+      }
+    },
+
+    watch: {
+      files: ['<%= jshint.files %>', 'app/**', 'vendor/**'],
+      tasks: ['copy', 'jshint', 'neuter', 'less', 'cssmin']
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-neuter');
+
+  grunt.registerTask('default', ['copy', 'jshint', 'neuter', 'less', 'cssmin', 'watch']);
+  grunt.registerTask('build', ['copy', 'jshint', 'neuter', 'less', 'cssmin']);
+};
---
+++ app/app.js
@@ -0,0 +1,15 @@
+require('vendor/javascripts/jquery-1.9.1');
+require('vendor/javascripts/handlebars-1.0.0-rc.3');
+require('vendor/javascripts/ember-1.0.0-rc.2');
+
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  // put your routes here
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});
