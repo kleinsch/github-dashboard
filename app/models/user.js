f7abcba313e9571e1e3b5a7d8f698f9a4b2a80eb
Nick Kleinschmidt <nick.kleinschmidt@gmail.com>
2013-04-05 13:39:57 -0700

    Quick visual cleanup

--- app/models/user.js
+++ app/models/user.js
@@ -3,11 +3,19 @@ require('app/models/repository');
 App.User = DS.Model.extend({
   login: DS.attr('string'),
   name: DS.attr('string'),
+  gravatar_id: DS.attr('string'),
+  public_repo_count: DS.attr('number'),
+  followers_count: DS.attr('number'),
+
   repositories: DS.hasMany('App.Repository'),
 
   displayName: function() {
     return this.get('name') ? this.get('name') : this.get('login');
-  }.property('name', 'login')
+  }.property('name', 'login'),
+
+  gravatarURL: function() {
+    return "http://www.gravatar.com/avatar/%@".fmt(this.get('gravatar_id'));
+  }.property('gravatar_id')
 });
 
 App.User.FIXTURES = [];
